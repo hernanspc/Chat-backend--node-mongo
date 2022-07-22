3951628de66ca05735c46315d1e4fb18cabb9785
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-07-22 15:55:00 -0500

    validarCampos js

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,20 +7,11 @@ const crearUsuario = async (req, res = response) => {
 
     res.json({
         ok: true,
-        usuario: "ABC"
+        usuario: "new"
     })
 }
 
 const login = async (req, res = response) => {
-
-    const errores = validationResult(req);
-
-    if (!errores.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        })
-    }
 
     const { email, password } = req.body;
     // const body = req.body;
--- router/auth.js
+++ router/auth.js
@@ -5,6 +5,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 //Controladores
 const { crearUsuario, renewToken, login } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -15,6 +16,7 @@ router.post('/new', crearUsuario)
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
 ], login)
 
 // Revalidar Token
