fd64f1fb673f0fb19dead10858bc648c8c40f749
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-07-24 21:44:04 -0500

    acs

--- controllers/mensajes.js
+++ controllers/mensajes.js
@@ -17,7 +17,7 @@ const obtenerChat = async (req, res) => {
             },
         ]
     })
-        .sort({ createdAt: 'desc' })
+        .sort({ createdAt: 'asc' })
         .limit(30);
 
     res.json({
